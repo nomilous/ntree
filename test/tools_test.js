d7c04768d6c7f6344dc11ec584a924a398fa3cbb
nomilous <nomilous@gmail.com>
2016-01-03 23:56:56 +0200

    build tree

--- test/tools_test.js
+++ test/tools_test.js
@@ -5,13 +5,11 @@ objective('Tools', function() {
   });
 
   context('constructor', function() {
-
     it('creates tree property from first arg', function(done, Tools, expect) {
       var tools = new Tools('TREE');
       expect(tools.tree).to.equal('TREE');
       done();
     });
-
   });
 
   context('readdirStatAsync()', function() {
@@ -41,6 +39,109 @@ objective('Tools', function() {
         expect(sources[0].stat.isDirectory).to.be.an.instanceof(Function);
         done();
       }).catch(done);
+    });
+  });
+
+  context('getNested()', function() {
+    it('retrieves object from given route in tree', function(done, Tools, expect) {
+      var tools = new Tools();
+      var tree = {
+        outer: {
+          nested: {
+            value: 1
+          }
+        }
+      }
+      var route = ['outer', 'nested', 'value'];
+      expect(tools.getNested(route, tree)).to.equal(1);
+      done();
+    });
+
+    it('thows if the route does not exist', function(done, Tools, expect) {
+      var tools = new Tools();
+      var tree = {
+        outer: {}
+      }
+      var route = ['outer', 'nested', 'value'];
+      try {
+        tools.getNested(route, tree)
+      } catch (e) {
+        expect(e.message).to.equal('getNested() missing route: outer/nested/value');
+        done();
+      }
+    });
+
+    it('does not throw if the last key is missing', function(done, Tools, expect) {
+      var tools = new Tools();
+      var tree = {
+        outer: {
+          nested: {
+            
+          }
+        }
+      }
+      var route = ['outer', 'nested', 'value'];
+      expect(tools.getNested(route, tree)).to.equal(undefined);
+      done();
+    })
+  });
+
+  context('setNested()', function() {
+    it('sets a value at route in tree', function(done, Tools, expect) {
+      var tools = new Tools();
+      var tree = {
+        outer: {
+          nested: {
+            
+          }
+        }
+      }
+      var route = ['outer', 'nested', 'value'];
+      tools.setNested(route, tree, 'VALUE');
+      expect(tree.outer.nested.value).to.equal('VALUE');
+      done();
+    });
+
+    it('throws if parent is missing', function(done, Tools, expect) {
+      var tools = new Tools();
+      var tree = {
+        outer: {
+          
+        }
+      }
+      var route = ['outer', 'nested', 'value'];
+      try {
+        tools.setNested(route, tree, 'VALUE');
+      } catch (e) {
+        expect(e.message).to.equal('setNested() missing route: outer/nested/value');
+        done();
+      }
+    });
+  });
+
+  context('setPropertyNested()', function() {
+    it('creates a property at route in tree referencing vertex', function(done, Tools, expect) {
+      var tools = new Tools();
+      var tree = {
+        outer: {
+          nested: {
+
+          }
+        }
+      }
+      var route = ['outer', 'nested', 'value'];
+      var vertex = mock({}).does(
+        function getValue() {
+          return 919;
+        },
+        function setValue(v) {
+          expect(v).to.equal(919);
+          done();
+        }
+      )
+      tools.setPropertyNested(route, tree, vertex);
+      expect(tree.outer.nested.value).to.equal(919);
+      tree.outer.nested.value = 919;
     });
   });
 
