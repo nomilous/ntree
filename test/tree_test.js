26529b79a62eb515700f4ab9d2b0521a6758a390
nomilous <nomilous@gmail.com>
2016-01-03 14:56:47 +0200

    attach source

--- test/tree_test.js
+++ test/tree_test.js
@@ -19,11 +19,23 @@ objective('Tree', function() {
       expect(tree._opts).to.equal(opts);
     });
 
+    it('creates sourceMask regex for stripping absolute paths to treePath', function(Tree, expect) {
+      var opts = {};
+      var tree = new Tree(opts);
+      expect(tree._opts.sourceMask).to.be.an.instanceof(RegExp);
+    });
+
     it('creates a property for tools', function(Tree, expect, Tools) {
       var opts = {};
       var tree = new Tree(opts);
       expect(tree._tools).to.be.an.instanceof(Tools);
       expect(tree._tools.tree).to.equal(tree);
+    });
+
+    it('creates a property for vertices', function(Tree, expect) {
+      var opts = {};
+      var tree = new Tree(opts);
+      expect(tree._vertices).to.eql({});
     });
 
     it('creates a property for serializers and registers defaults', function(done, Tree, Javascript, Directory, expect) {
@@ -56,11 +68,15 @@ objective('Tree', function() {
       done();
     });
 
-    it('subscribes to $load for _assemble()', function(done, Tree, expect) {
+    it('subscribes _attachSource() to $load for _assemble()', function(done, Tree, expect) {
       var opts = {};
+      mock(Tree.prototype).does(function _attachSource(source) {
+        expect(source).to.equal('SOURCE');
+        done();
+      });
       var tree = new Tree(opts);
       expect(tree._emitter._events['$load']).to.be.an.instanceof(Function);
-      done();
+      tree._emitter.emit('$load', 'SOURCE');
     });
 
   });
@@ -90,5 +106,61 @@ objective('Tree', function() {
     });
   });
 
+  context('_attachSource()', function() {
+
+    beforeEach(function(Tree) {
+      mock('tree', new Tree({mount: '/path/to/tree'}));
+      mock('sourceDir', {
+        filename: '/path/to/tree/branch/leaf',
+        stat: {
+          isDirectory: function() {
+            return true;
+          }
+        }
+      });
+      mock('sourceFile', {
+        filename: '/path/to/tree/branch/leaf.js',
+        stat: {
+          isDirectory: function() {
+            return false;
+          }
+        }
+      });
+    });
+
+    it('directs ammended source to _attachDirectory()', function(done, tree, sourceDir, expect) {
+      tree.does(function _attachDirectory(source) {
+        expect(source.treePath).to.equal('branch/leaf');
+        done();
+      });
+      tree._attachSource(sourceDir);
+    });
+
+    it('directs ammended source to _attachFile()', function(done, tree, sourceFile, expect) {
+      tree.does(function _attachFile(source) {
+        expect(source.treePath).to.equal('branch/leaf');
+        expect(source.ext).to.equal('.js');
+        expect(source.serializer).to.equal(tree._serializers['.js']);
+        done();
+      });
+      tree._attachSource(sourceFile);
+    });
+
+  });
+
+  context('_attachDirectory()', function() {
+
+    it('attaches a vertex with agent to the tree', function(done) {
+      done();
+    });
+
+  });
+  context('_attachFile()', function() {
+
+    it('attaches a vertex with agent to the tree', function(done) {
+      done();
+    });
+
+  });
 
 });
